e93161120c35f65bebafe42a1a8f399f25ac9496
VadymSchastlyvtsev <drdre6870@gmail.com>
2023-07-18 23:06:57 +0300

    sdasd
    sdasd

--- src/index.js
+++ src/index.js
@@ -16,74 +16,93 @@ catInfo: document.querySelector(".cat-info"),
   const {loader, error, catInfo, loaderText, select} = selectors;
 
 
-  function createMarkupCat(arr) {
-    return arr
-      .map(
-        ({ breeds: [{ name, description, temperament }], url }) =>
-          `<div class="cat-info-img">
-                <img src="${url}" alt="${name}" width="500"/>
-           </div>
-           <div class="cat-info-descr">
-                <h2 class="cat-info-title">${name}</h2>
-                <p class="cat-info-text">${description}</p>
-                <p class="cat-info-temperament"><span>Temperament: </span>${temperament}</p>
-           </div>`
-      )
-      .join('');
-  }
-
- function showError() {
-    Notiflix.Notify.failure('Oops! Something went wrong! Try reloading the page!');
-  }
-
-  function hideError() {
-    error.style.display = "none";
-  };
-
-  function hideLoader() {
-    select.style.display = "block";
-    loader.style.display = "none";
-    loaderText.style.display = "none";
-  };
-
-  function showLoader() {
-    loader.style.display = "block";
-    loaderText.style.display = "block";
-
-    fetchBreeds()
+  fetchBreeds()
   .then(data => {
     select.innerHTML = createMarkup(data);
-    hideLoader();
+    showLoader('endStartInit');
   })
   .catch(() => {
-    showError();
+    messageError();
   });
+  // fetchBreeds()
+  // .then(data => {
+  //   console.dir(data);
+  //   // select.innerHTML = createMarkup(data);
+  // })
+  // .catch(() => {
+  //   showError();
+  // })
+//   .finally(() => {
+//     hideLoader();
+//   });
+
+
+//  function createMarkup(arr) {
+//     return arr
+//     .map(({id, name }) => `<option value="${id}">${name}</option>`)
+//     .join('')
+// };
+
+
+//   function createMarkupCat(arr) {
+//     return arr
+//       .map(
+//         ({ breeds: [{ name, description, temperament }], url }) =>
+//           `<div class="cat-info-img">
+//                 <img src="${url}" alt="${name}" width="500"/>
+//            </div>
+//            <div class="cat-info-descr">
+//                 <h2 class="cat-info-title">${name}</h2>
+//                 <p class="cat-info-text">${description}</p>
+//                 <p class="cat-info-temperament"><span>Temperament: </span>${temperament}</p>
+//            </div>`
+//       )
+//       .join('');
+//   };
+
+ 
+
+
+
+// select.addEventListener('change', onChange);
+
+// function onChange(evt) {
+//     showLoader()
+//     hideError();
+//     const id = evt.currentTarget.value;
+
+//     fetchCatByBreed(id)
+//     .then(data => {
+//         if (!data.lendth) {
+//             showError()
+//         }
+//         catInfo.innerHTML = createMarkupCat(data);
+//         showLoader();
+//     })
+//     .catch(() => {
+//         showError();
+//     })
+// };
+
+
+function showError() {
+  Notiflix.Notify.failure('Oops! Something went wrong! Try reloading the page!');
 }
 
- function createMarkup(arr) {
-    return arr
-    .map(({id, name }) => `<option value="${id}">${name}</option>`)
-    .join('')
+function hideError() {
+  error.style.display = "none";
 };
 
-select.addEventListener('change', onChange);
+function hideLoader() {
+  select.style.display = "block";
+  loader.style.display = "none";
+  loaderText.style.display = "none";
+};
 
-function onChange(evt) {
-    showLoader()
-    hideError();
-    const id = evt.currentTarget.value;
-
-    fetchCatByBreed(id)
-    .then(data => {
-        if (!data.lendth) {
-            showError()
-        }
-        catInfo.innerHTML = createMarkupCat(data);
-        showLoader();
-    })
-    .catch(() => {
-        showError();
-    })
-}
+function showLoader() {
+  showLoader();
+};
 
 Notiflix.Notify.init();
+
+export {showError}
--- src/partials/api-caties.js
+++ src/partials/api-caties.js
@@ -1,22 +1,29 @@
-import axios from 'axios';
+import axios from "axios";
+import { showError } from "../index";
 
-axios.defaults.baseURL = 'https://api.thecatapi.com/v1';
+axios.defaults.baseURL = "https://api.thecatapi.com/v1";
 axios.defaults.headers.common['x-api-key'] =
-  'live_XXkZNKxlIGpPljBffFGmd2H84qKwpAvy2f5RSXkd9IySz5kMxihP8k5eKqhuKlNi';
+  "live_XXkZNKxlIGpPljBffFGmd2H84qKwpAvy2f5RSXkd9IySz5kMxihP8k5eKqhuKlNi";
 
 
-  function fetchBreeds() {
-    return axios.get('/breeds').then(resp => {
-      if (resp.status !== 200) {
-        throw new Error(resp.statusText);
-      }
+  async function fetchBreeds() {
+    try {
+      const resp = await axios.get('/breeds');
       return resp.data;
-    });
+    } catch (error) {
+      showError('Oops! Something went wrong! Try reloading the page!')
+    }
+    // return axios.get('/breeds').then(resp => {
+    //   if (!resp.ok) {
+    //     throw new Error(resp.statusText);
+    //   }
+    //   return resp.data;
+    // });
   }
 
   function fetchCatByBreed(breedId) {
     return axios.get(`/images/search?breed_ids=${breedId}`).then(resp => {
-      if (resp.status !== 200) {
+      if (!resp.ok) {
         throw new Error(resp.statusText);
       }
       return resp.data;
